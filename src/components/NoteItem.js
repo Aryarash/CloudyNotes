ec17c94ee0c92c478f2c987435f6325ce1a9b089
aryarash <shriramt325@gmail.com>
2023-08-26 11:59:30 +0530

    update

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -16,15 +16,13 @@ const NoteItem = (props) => {
     };
     return (
         <div className='col-md-3'>
-            <div className="card">
+            <div className="card my-3">
                 <div className="card-body">
                     <span className='position-absolute top-0 translate-middle badge rounded-pill bg-success' style={{left:'90%',zIndex:'1'}}>{note.tag!==""?(note.tag.length>15?note.tag.substring(0,15)+'...':note.tag):""}</span>
-                    <div className='d-flex align-items-center'>
-                        <h5 className="card-title">{note.title}</h5>
-                        <i className="fa-sharp fa-solid fa-trash mx-2" onClick={handleDelete}></i>
-                        <i className="fa-solid fa-file-pen mx-2" onClick={handleEdit}></i>
-                    </div>
+                    <p className="card-title"><strong>{note.title}</strong></p>
                     <p className="card-text">{note.description}</p>
+                    <i className="fa-sharp fa-solid fa-trash mx-2" onClick={handleDelete}></i>
+                    <i className="fa-solid fa-file-pen mx-2" onClick={handleEdit}></i>
                 </div>
             </div>
         </div>
